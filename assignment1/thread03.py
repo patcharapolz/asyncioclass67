5bb9651bd86c3a2869b7fc2bfb140ebaf5b2c213
usar <cffemate@gmail.com>
2024-06-19 12:20:45 +0700

    submit 03-09 assignment

--- assignment1/thread03.py
+++ assignment1/thread03.py
@@ -1,1 +1,24 @@
 # extending the Thread class
+from time import sleep, ctime
+from threading import Thread
+
+# a custom fuction that blocks for a moment
+class CustomThread(Thread):
+    # override the fun function
+    def run(self):
+        # block for a moment
+        sleep(1)
+
+        # display a message
+        print(f"{ctime()} This is coming from another thread")
+
+# create a thread
+thread = CustomThread()
+
+# run the thread
+thread.start()
+
+# wait for the thread to finish
+print(f"{ctime()} Waiting for the thread...")
+
+thread.join()
--- assignment1/thread04.py
+++ assignment1/thread04.py
@@ -1,1 +1,31 @@
 # extending the Thread class and return values
+from time import sleep, ctime
+from threading import Thread
+
+# a custom fuction that blocks for a moment
+class CustomThread(Thread):
+    # override the fun function
+    def run(self):
+        # block for a moment
+        sleep(1)
+
+        # display a message
+        print(f"{ctime()} This is coming from another thread")
+
+        # store return value
+        self.value = 99
+
+# create a thread
+thread = CustomThread()
+
+# run the thread
+thread.start()
+
+# wait for the thread to finish
+print(f"{ctime()} Waiting for the thread...")
+
+thread.join()
+
+# get the value returned from run
+value = thread.value
+print(f"{ctime()} Got: {value}")
