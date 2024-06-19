ef5af2652169c4871f88057bc050049c82e08b98
usar <cffemate@gmail.com>
2024-06-19 11:43:41 +0700

    submit 01 assignment

--- assignment1/thread01.py
+++ assignment1/thread01.py
@@ -12,8 +12,11 @@ def task():
 
 # create a thread
 thread = Thread(target=task)
+
 # run the thread
 thread.start()
+
 # wait for the thread to finish
 print(f"{ctime()} Waiting for the thread...")
+
 thread.join()
