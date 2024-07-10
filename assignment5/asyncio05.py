422df53441c901bae90b575a52ac321069a6b429
usar <cffemate@gmail.com>
2024-07-10 14:47:09 +0700

    modify asyncio 5

--- assignment5/asyncio05.py
+++ assignment5/asyncio05.py
@@ -4,63 +4,41 @@ from os import system
 import asyncio
 
 # coroutine to execute in a new task
-async def rice():
-    # generate a random value between 0 and 1
+async def task_coro(menu):
+    # generate a random value between 0 and 1 and plus 1
     value = random() + 1
 
-    print(f">Menu Rice cooking {value} sec")
+    print(f"Microwave {menu} cooking {value} second")
 
     # block for a moment
     await asyncio.sleep(value)
 
     # report the value
-    print(f">Menu Rice finish")
+    print(f"Microwave {menu} finish")
 
-async def noodle():
-    # generate a random value between 0 and 1
-    value = random() + 1
-
-    print(f">Menu Noodle cooking {value} sec")
-
-    # block for a moment
-    await asyncio.sleep(value)
-
-    # report the value
-    print(f">Menu Noodle finish")
-
-async def curry():
-    # generate a random value between 0 and 1
-    value = random() + 1
-
-    print(f">Menu Curry cooking {value} sec")
-
-    # block for a moment
-    await asyncio.sleep(value)
-
-    # report the value
-    print(f">Menu Curry finish")
+    return value
 
 # main coroutine
 async def main():
     system("cls")
 
     # create many tasks
-    makeRice = asyncio.create_task(rice(), name= "Rice")
-    makeNoodle = asyncio.create_task(noodle(), name= "Noodle")
-    makeCurry = asyncio.create_task(curry(), name= "Curry")
+    Menus = [asyncio.create_task(task_coro("Rice"), name = "Rice"),
+             asyncio.create_task(task_coro("Noodle"), name = "Noodle"),
+             asyncio.create_task(task_coro("Curry"), name = "Curry")]
 
-    Menus = [makeRice, makeNoodle, makeCurry]
-
-    # wait for all tasks to complete
+    # wait for first tasks to complete
     done, pending = await asyncio.wait(Menus, return_when = asyncio.FIRST_COMPLETED)
 
     # get the first task to complete
     print(f"\nTask Complete: {len(done)}")
 
-    first = done.pop()
+    complete_menu = done.pop()
 
-    print(f"\t{first.get_name()} is complete.")
-    print(f"\nTask Uncomplete: {len(pending)}")
+    print(f" - {complete_menu.get_name()} is complete in {complete_menu.result()} second.")
+    print(f"Task Uncomplete: {len(pending)}")
+    for uncomplete_menu in pending:
+        print(f" - {uncomplete_menu.get_name()} is uncomplete")
    
 
 # start the asyncio program
