811a27926acf81f94619007f1b24313194be0f92
usar <cffemate@gmail.com>
2024-07-10 11:19:16 +0700

    finish

--- assignment5/asyncio05.py
+++ assignment5/asyncio05.py
@@ -1,12 +1,10 @@
 from random import random
 from time import sleep, time
+from os import system
 import asyncio
 
 # coroutine to execute in a new task
-async def task_coro(arg):
-    # create menu
-    food = ["Rice", "Noodle", "Curry"]
-
+async def rice():
     # generate a random value between 0 and 1
     value = random() + 1
 
@@ -14,18 +12,51 @@ async def task_coro(arg):
     await asyncio.sleep(value)
 
     # report the value
-    print(f">Menu {food[arg]} finish in {value:.2f} sec")
+    print(f">Menu Rice finish in {value} sec")
+async def noodle():
+    # generate a random value between 0 and 1
+    value = random() + 1
+
+    # block for a moment
+    await asyncio.sleep(value)
+
+    # report the value
+    print(f">Menu Noodle finish in {value} sec")
+
+async def curry():
+    # generate a random value between 0 and 1
+    value = random() + 1
+
+    # block for a moment
+    await asyncio.sleep(value)
+
+    # report the value
+    print(f">Menu Curry finish in {value} sec")
 
 # main coroutine
 async def main():
+    system("cls")
+
     # create many tasks
-    tasks = [asyncio.create_task(task_coro(i)) for i in range(3)]
+    makeRice = asyncio.create_task(rice(), name= "Rice")
+    makeNoodle = asyncio.create_task(noodle(), name= "Noodle")
+    makeCurry = asyncio.create_task(curry(), name= "Curry")
+
+    Menus = [makeRice, makeNoodle, makeCurry]
 
     # wait for all tasks to complete
-    done, pending = await asyncio.wait(tasks, return_when = asyncio.FIRST_COMPLETED)
+    done, pending = await asyncio.wait(Menus, return_when = asyncio.FIRST_COMPLETED)
 
-    # report results
-    print("Done")
+    await makeRice
+    await makeNoodle
+    await makeCurry
+
+    # get the first task to complete
+    print(f"\nTask Complete: {len(done)}")
+    first = done.pop()
+    print(f"\t{first.get_name()} finish first.")
+    print(f"\nTask Uncomplete: {len(pending)}")
+   
 
 # start the asyncio program
 asyncio.run(main())
