8386f313b71a008cbd853df767bb544a52438372
usar <cffemate@gmail.com>
2024-08-08 22:20:10 +0700

    fix assignment03

--- assignment3/asyncio03.py
+++ assignment3/asyncio03.py
@@ -1,8 +1,13 @@
 # example of creating an event loop
 import asyncio
 
-# Create and access a new asyncio event loop
+async def some_async_task():
+    print("Sleeping for 1 seconds")
+    await asyncio.sleep(1)
+    print(1)
+
+# Get the current event loop.
 loop = asyncio.new_event_loop()
 
-# Report defaults of the loop
-print(loop)
+# Run until the coroutine is completed
+loop.run_until_complete(some_async_task())
